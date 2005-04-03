f8c35dc931f0bccbd4ced10df7dacb31557e9ca2
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2005-04-03 23:33:16 +0000

    * Added support to specify path to nested bookmark. 	* Version number is 0.8.1.

--- trunk/PTYSession.m
+++ trunk/PTYSession.m
@@ -906,13 +906,17 @@ static NSString *PWD_ENVVALUE = @"~";
         if (oIdleCount>200&&!waiting) {
             waiting=YES;
             if (REFRESHED)
-                [tabViewItem setLabelAttributes: idleStateAttribute];
+			{
+				[tabViewItem setLabelAttributes: idleStateAttribute];
+				[NSApp requestUserAttention: NSInformationalRequest];
+			}
             else
                 [tabViewItem setLabelAttributes: normalStateAttribute];
         }
         else if (waiting&&oIdleCount<=200) {
             waiting=NO;
             [tabViewItem setLabelAttributes: newOutputStateAttribute];
+			[NSApp requestUserAttention: NSInformationalRequest];
         }
     }
     else {
